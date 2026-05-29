d5972af01c3fa9c31c3ad1f37d1dbf51a40ea989
Yonggang142 <yonggang2004@gmail.com>
2026-05-29 14:05:51 +0800

    Added calculator logic and buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { createContext, useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [output, setOutput] = useState("")
+
+  const [evaluated, setEvaluated] = useState(false)
+
+
+  const registers = {0: "0", 1: "1", 2: "2", 3: "3", 
+    4: "4", 5: "5", 6: "6", 7: "7", 
+    8: "8", 9: "9", 10: "+", 
+    11: "-", 12: "*", 13: "/", 14: "="}
+
+  function evaluate() {
+    try {
+      const result = eval(output)
+      setOutput(result)
+      setEvaluated(true)
+    } catch (error) {
+
+      setOutput("Error")
+      setEvaluated(true)
+    }
+  }
+
+  function handleClick(x) {
+    if (x == 14) {
+      evaluate()
+      return 
+    }
+
+    const char = registers[x]
+
+
+    if (evaluated == true) {
+      setOutput(char)
+      setEvaluated(false)
+    } else {
+      setOutput(prev => prev + char)
+    }
+  } 
 
   return (
     <>
       <div>
-        <h1>
-          hello
+        <h1 className='Title'>
+          Calculator App
           
         </h1>
+
+        <h2 className='Output'>
+          {output}
+        </h2>
+      
+        <div>
+					<button className = "button" onClick={() => handleClick(0)}> 0 </button>
+					<button className = "button" onClick={() => handleClick(1)}> 1 </button>
+					<button className = "button" onClick={() => handleClick(2)}> 2 </button>
+					<button className = "button" onClick={() => handleClick(3)}> 3 </button>
+					<button className = "button" onClick={() => handleClick(4)}> 4 </button>
+					<button className = "button" onClick={() => handleClick(5)}> 5 </button>
+					<button className = "button" onClick={() => handleClick(6)}> 6 </button>
+					<button className = "button" onClick={() => handleClick(7)}> 7 </button>
+					<button className = "button" onClick={() => handleClick(8)}> 8 </button>
+					<button className = "button" onClick={() => handleClick(9)}> 9 </button>
+					<button className = "button" onClick={() => handleClick(10)}> + </button>
+					<button className = "button" onClick={() => handleClick(11)}> - </button>
+					<button className = "button" onClick={() => handleClick(12)}> * </button>
+					<button className = "button" onClick={() => handleClick(13)}> / </button>
+					<button className = "button" onClick={() => handleClick(14)}> = </button>
+    		</div>
+    
+
+
+
+
+
       </div>
     </>
   )
